d370eed7b4e2ea005b4c9edaaa272ba3bb9c22f0
tykwon0422 <101312801+tykwon0422@users.noreply.github.com>
2022-05-24 22:37:17 +0900

    Update Pyrebase_STT.py

--- source_db/Pyrebase_STT.py
+++ source_db/Pyrebase_STT.py
@@ -6,30 +6,29 @@ from difflib import SequenceMatcher
 
 class STT:
     def __init__(self):
-        firebaseConfig = {
-        #insert your firebase config
-        }
-
         firebase = pyrebase.initialize_app(firebaseConfig)
         # Get Database
         self.db = firebase.database()
+
+        self.recognizer = sr.Recognizer()
         self.mic = sr.Microphone()
+
+        with self.mic as source:
+            self.recognizer.adjust_for_ambient_noise(source)
 
     def run(self):
         # Obtain audio from the microphone
-        r = sr.Recognizer()
         with self.mic as source:
-            r.adjust_for_ambient_noise(source)
             print("Say something!")
             try:
-                audio = r.listen(source, timeout=5)
+                audio = self.recognizer.listen(source, timeout=5)
             except:
                 print("시간이 초과되었습니다.")
                 return
 
         # STT
         try:
-            input = r.recognize_google(audio, language='ko')
+            input = self.recognizer.recognize_google(audio, language='ko')
             print(input)
         except sr.UnknownValueError:
             print("음성을 인식하지 못 했습니다.")
@@ -46,9 +45,9 @@ class STT:
                 continue
             rooms = self.db.child(floor.key()).get()
             for room in rooms.each():
-                score = max(SequenceMatcher(None, room.val()["name"], input).ratio(), 
-                            SequenceMatcher(None, room.val()["name"], input + "연구실").ratio())
-                if score > 0.6 or (room.val()["charge"] in input) or (room.key() in input):
+                score = SequenceMatcher(None, room.val()["name"], input).ratio()
+                if (score > 0.6 or (room.val()["charge"] in input)
+                    or (room.key() in input) or (input in room.val["name"])):
                     information = [room.key()] + [i for i in room.val().values()]
                     room_list.append(information)
 
